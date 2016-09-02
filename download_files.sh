4904ae6b387b22451ff1f489e29b4781b7867176
Shafique Jamal <shafique.jamal@gmail.com>
2016-09-02 15:20:20 -0400

    Specify output format for ssconvert

--- download_files.sh
+++ download_files.sh
@@ -7,10 +7,10 @@ do
 	echo "+++++++++++++++++++++"
 	while IFS=, read number name code; do
   		OUTPUTPATH="${ISHARES_DATA_DIR}/${code}_${number}_${name}"
-  		rm -f "${OUTPUTPATH}.*"
+      rm "${OUTPUTPATH}"*
   		wget -c "https://www.blackrock.com/ca/individual/en/$number/fund-download.dl" -O "${OUTPUTPATH}.xls"
   		cat "${OUTPUTPATH}.xls" | perl -0777 -pe 's#</Style>#</ss:Style>#sg' > "${OUTPUTPATH}2.xls"
-  		ssconvert -S "${OUTPUTPATH}2.xls" "${OUTPUTPATH}.csv"
+  		ssconvert -S --export-type=Gnumeric_stf:stf_csv "${OUTPUTPATH}2.xls" "${OUTPUTPATH}.csv"
   		
   		for CSVFILE in ${OUTPUTPATH}.csv.*; 
   		do
